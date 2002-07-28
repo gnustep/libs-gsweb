37d20cd40158bd33690645f6384a17e736513386
Manuel Guesdon <mguesdon.gna@orange-concept.com>
2002-07-28 12:52:49 +0000

    2002-07-28   Manuel Guesdon <mguesdon@orange-concept.com> 	* GSWeb.framework/WebObjects: 		o added WebObjects headers (suggested by David Wetzel <dave@turbocat.de>) 	* GSWeb.framework/GNUmakefile: 		o added WebObjects headers (suggested by David Wetzel <dave@turbocat.de>) 	* GSWeb.framework/makefile.postamble: 		o added copy of WebObjects headers (suggested by David Wetzel <dave@turbocat.de>)
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/gsweb/trunk@14206 72102866-910b-0410-8b05-ffd578937521

---
+++ GSWeb.framework/WebObjects/WORequest.h
@@ -0,0 +1,38 @@
+/** WORequest.h - <title>GSWeb: Compatibility Header - WORequest</title>
+
+   Copyright (C) 2002 Free Software Foundation, Inc.
+   
+   Written by:	Manuel Guesdon <mguesdon@orange-concept.com>
+
+   Date:        Jul 2002
+   
+   $Revision$
+   $Date$
+
+   This file is part of the GNUstep Web Library.
+   
+   <license>
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+   
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+   
+   You should have received a copy of the GNU Library General Public
+   License along with this library; if not, write to the Free
+   Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+   </license>
+**/
+
+// $Id$
+
+#ifndef _WORequest_h__
+	#define _WORequest_h__
+
+#include <GSWeb/GSWeb.h>
+
+#endif
