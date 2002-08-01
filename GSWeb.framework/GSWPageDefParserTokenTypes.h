d295aeb3ce0c29e580beafbde960096339cfa4a3
Manuel Guesdon <mguesdon.gna@orange-concept.com>
2002-08-01 12:36:56 +0000

    added GSWPageDefParserTokenTypes.h added GSWPageDefParserTokenTypes.txt
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/gsweb/trunk@14219 72102866-910b-0410-8b05-ffd578937521

---
+++ GSWeb.framework/GSWPageDefParserTokenTypes.h
@@ -0,0 +1,47 @@
+#ifndef INC_GSWPageDefParserTokenTypes_h_
+#define INC_GSWPageDefParserTokenTypes_h_
+
+/*
+ * ANTLR-generated file resulting from grammar PageDef.g
+ * 
+ * Terence Parr, MageLang Institute
+ * with John Lilley, Empathy Software
+ * and Manuel Guesdon, Software Builders
+ * ANTLR Version 2.5.0; 1996,1997,1998,1999
+ */
+
+
+#include <GSWeb/GSWeb.h>
+
+enum GSWPageDefParserTokenTypes {
+	GSWPageDefParserTokenType_EOF_ = 1,
+	GSWPageDefParserTokenType_NULL_TREE_LOOKAHEAD = 3,
+	GSWPageDefParserTokenType_INCLUDE = 4,
+	GSWPageDefParserTokenType_WS = 5,
+	GSWPageDefParserTokenType_STRING = 6,
+	GSWPageDefParserTokenType_IDENT = 7,
+	GSWPageDefParserTokenType_COLUMN = 8,
+	GSWPageDefParserTokenType_LCURLY = 9,
+	GSWPageDefParserTokenType_RCURLY = 10,
+	GSWPageDefParserTokenType_SEMI = 11,
+	GSWPageDefParserTokenType_ASSIGN = 12,
+	GSWPageDefParserTokenType_INT = 13,
+	GSWPageDefParserTokenType_YES = 14,
+	GSWPageDefParserTokenType_NO = 15,
+	GSWPageDefParserTokenType_HEXNUM = 16,
+	GSWPageDefParserTokenType_CIRC = 17,
+	GSWPageDefParserTokenType_TILDE = 18,
+	GSWPageDefParserTokenType_IDENTREF = 19,
+	GSWPageDefParserTokenType_PIDENT = 20,
+	GSWPageDefParserTokenType_PIDENTREF = 21,
+	GSWPageDefParserTokenType_SL_COMMENT = 22,
+	GSWPageDefParserTokenType_ML_COMMENT = 23,
+	GSWPageDefParserTokenType_POINT = 24,
+	GSWPageDefParserTokenType_ESC = 25,
+	GSWPageDefParserTokenType_HEXINT = 26,
+	GSWPageDefParserTokenType_DIGIT = 27,
+	GSWPageDefParserTokenType_HEXDIGIT = 28,
+	GSWPageDefParserTokenType_LCLETTER = 29,
+	GSWPageDefParserTokenType_LETTER = 30,
+};
+#endif /*INC_GSWPageDefParserTokenTypes_h_*/
