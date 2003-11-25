c4c619464c1081bc6a2be7e6216f3e53d47f711d
Manuel Guesdon <mguesdon.gna@orange-concept.com>
2003-11-25 13:11:38 +0000

    o added (come from Dave Lopper)
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/gsweb/trunk@18196 72102866-910b-0410-8b05-ffd578937521

---
+++ GSWExtensions.framework/GSWKeyValueConditional.h
@@ -0,0 +1,44 @@
+/** GSWKeyValueConditional.h - <title>GSWeb: Class GSWKeyValueConditional</title>
+
+   Copyright (C) 2003 Free Software Foundation, Inc.
+   
+   Written by:	Dave Lopper
+   Date: 	Mar 2003
+   
+   $Revision$
+   $Date$
+   
+   <abstract></abstract>
+
+   This file is part of the GNUstep Web Library.
+
+   <license>
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+   
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+   
+   You should have received a copy of the GNU Library General Public
+   License along with this library; if not, write to the Free
+   Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+   </license>
+**/
+
+#ifndef _GSWKeyValueConditional_h__
+#define _GSWKeyValueConditional_h__
+
+#include <GSWeb/GSWComponent.h>
+
+//==============================================================================
+@interface GSWKeyValueConditional : GSWComponent
+{
+}
+
+@end
+
+#endif //_GSWKeyValueConditional_h__
---
+++ GSWExtensions.framework/GSWKeyValueConditional.m
@@ -0,0 +1,59 @@
+/** GSWKeyValueConditional.m - <title>GSWeb: Class GSWKeyValueConditional</title>
+
+   Copyright (C) 2003 Free Software Foundation, Inc.
+   
+   Written by:	Dave Lopper
+   Date: 	Mar 2003
+   
+   $Revision$
+   $Date$
+   
+   <abstract></abstract>
+
+   This file is part of the GNUstep Web Library.
+
+   <license>
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+   
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+   
+   You should have received a copy of the GNU Library General Public
+   License along with this library; if not, write to the Free
+   Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+   </license>
+**/
+
+#include "config.h"
+
+RCS_ID("$Id$")
+
+#include "GSWExtWOCompatibility.h"
+#include "GSWKeyValueConditional.h"
+
+//===================================================================================
+@implementation GSWKeyValueConditional
+
+- (BOOL) synchronizesVariablesWithBindings
+{
+    return NO;
+}
+
+- (BOOL) condition
+{
+    id	key = [self valueForBinding:@"key"];
+    id	parentValue = (key ? [[self parent] valueForKeyPath:key]:nil);
+    id	value = [self valueForBinding:@"value"];
+
+    if(parentValue == nil)
+        return value == nil;
+    else
+        return [parentValue isEqual:value];    
+}
+
+@end
