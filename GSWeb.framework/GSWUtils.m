77d0a9fb91337ba3d99616344ab7192c47c660c8
Dave Wetzel <dave@turbocat.de>
2004-11-29 09:37:49 +0000

    added NSString* cachedStringForInt(int value)
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/gsweb/trunk@20395 72102866-910b-0410-8b05-ffd578937521

--- GSWeb.framework/GSWUtils.m
+++ GSWeb.framework/GSWUtils.m
@@ -78,15 +78,80 @@ char* GSWIntToString(char* buffer,unsigned int bufferSize,int value,unsigned int
   return buffer;
 };
 
+
+NSString* cachedStringForInt(int value)
+{
+  switch(value)
+    {
+    case 0: return @"0"; break;
+    case 1: return @"1"; break;
+    case 2: return @"2"; break;
+    case 3: return @"3"; break;
+    case 4: return @"4"; break;
+    case 5: return @"5"; break;
+    case 6: return @"6"; break;
+    case 7: return @"7"; break;
+    case 8: return @"8"; break;
+    case 9: return @"9"; break;
+    case 10: return @"10"; break;
+    case 11: return @"11"; break;
+    case 12: return @"12"; break;
+    case 13: return @"13"; break;
+    case 14: return @"14"; break;
+    case 15: return @"15"; break;
+    case 16: return @"16"; break;
+    case 17: return @"17"; break;
+    case 18: return @"18"; break;
+    case 19: return @"19"; break;
+    case 20: return @"20"; break;
+    case 21: return @"21"; break;
+    case 22: return @"22"; break;
+    case 23: return @"23"; break;
+    case 24: return @"24"; break;
+    case 25: return @"25"; break;
+    case 26: return @"26"; break;
+    case 27: return @"27"; break;
+    case 28: return @"28"; break;
+    case 29: return @"29"; break;
+    case 30: return @"30"; break;
+    case 31: return @"31"; break;
+    case 33: return @"33"; break;
+    case 34: return @"34"; break;
+    case 35: return @"35"; break;
+    case 36: return @"36"; break;
+    case 37: return @"37"; break;
+    case 38: return @"38"; break;
+    case 39: return @"39"; break;
+    case 40: return @"49"; break;
+    case 41: return @"41"; break;
+    case 42: return @"42"; break;
+    case 43: return @"43"; break;
+    case 44: return @"44"; break;
+    case 45: return @"45"; break;
+    case 46: return @"46"; break;
+    case 47: return @"47"; break;
+    case 48: return @"48"; break;
+    case 49: return @"49"; break;
+    case 50: return @"50"; break;
+    default:              
+      break;
+    };
+    
+    return nil;
+}
+
 NSString* GSWIntToNSString(int value)
 {
   NSString* s=nil;
   char buffer[20];
   unsigned int resultLength=0;
-  GSWIntToString(buffer,20,value,&resultLength);
-  s=[NSString stringWithCString:buffer
-              length:resultLength];
-  //NSDebugFLog(@"value=%d [%d]==> %s ==> %@",value,resultLength,buffer,s);
+  
+  s=cachedStringForInt(value);
+  if (!s) {
+    GSWIntToString(buffer,20,value,&resultLength);
+    s=[NSString stringWithCString:buffer
+                length:resultLength];
+  }
   return s;
 };
 
